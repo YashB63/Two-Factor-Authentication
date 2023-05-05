6a9fbe8329b490afd34056c6ada511a54523d635
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2023-05-05 23:08:19 +0530

    Add files via upload

---
+++ Two Factor Authentication/two_factor_authentication.py
@@ -0,0 +1,54 @@
+import time
+import pyotp
+import qrcode
+
+#key = pyotp.random_base32()
+
+key = "ElitesYashSuperSecretKey"
+
+# totp = pyotp.TOTP(key)
+
+# print(totp.now())
+
+# # time.sleep(30)
+
+# # print(totp.now())
+
+
+# input_code = input("Enter 2 FA Code:")
+
+# # code = totp.now()
+# # print(input_code == code)
+
+# print(totp.verify(input_code))
+
+counter = 0
+
+hotp = pyotp.HOTP(key)
+
+print(hotp.at(0))
+print(hotp.at(1))
+print(hotp.at(2))
+print(hotp.at(3))
+print(hotp.at(4))
+
+for _ in range (5):
+    print(hotp.verify(input("Enter Code: "), counter))
+    counter += 1
+    
+#----------------Generate QR code------------
+
+# uri = pyotp.totp.TOTP(key).provisioning_uri(name="AhamBrahmasmi63",
+#                                             issuer_name="Yash App")
+
+# print(uri)
+
+# qrcode.make(uri).save("totp.png")
+
+#-----------------------------------------------------------------
+
+# totp = pyotp.TOTP(key)
+# print(totp)
+
+# while True:
+#     print(totp.verify(input("Enter code:")))
